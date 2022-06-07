caa8b834bd6cfcd4c6cc74b3d7998648983d4192
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-07 14:46:05 +0200

    Add fetching weather data and display it in summary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,13 +1,36 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+  
+  const [currentWeatherData, setCurrentWeatherData] = useState('');
+  const [pending, setPending] = useState(false);
+
+  const handleCityChange = useCallback(CITY_VALUE => {
+    setPending(true);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${CITY_VALUE}&appid=929757977b65f1e4d51dca184ecf5730&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     
+     const weatherData = {
+       city: data.name,
+       temp: Math.round(data.main.temp),
+       icon: data.weather[0].icon,
+       description: data.weather[0].main
+      };
+      setPending(false);
+      setCurrentWeatherData(weatherData);
+    });
+
+  }, []);
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
-      <Loader />
+      <PickCity action={handleCityChange}/>
+      {(currentWeatherData && pending===false) && <WeatherSummary {...currentWeatherData}/>}
+      {pending===true && <Loader />}
     </section>
   )
 };
